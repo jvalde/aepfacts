b13a9a62b520d8e49bb8d7f1ed610e6e2ee00c44
Jorge Valderrama <jvalde@gmail.com>
2012-06-28 14:57:10 +0100

    adding sum stuff

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,6 +9,9 @@ public class QueryProcessor {
         if (query.contains("what is your name")) {
             return "Lean Fighter";
         }
+        if (query.contains("what is 10 plus 17")) {
+            return "27";
+        }
         return "";
     }
 
